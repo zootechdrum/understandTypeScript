bb131efa2178d1ab1b5bc0b071140ebdb8cd5819
Cesar Gomez <zootechdrum@gmail.com>
2020-05-24 12:07:14 -0700

    tried to solve autobind issue with tutorial no luck

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
-//autobind decorater
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-function autobind(_, _2, descriptor) {
+// autobind decorator
+var autobind = function (_, _2, descriptor) {
     var originalMethod = descriptor.value;
     var adjDescriptor = {
         configurable: true,
@@ -15,14 +15,11 @@ function autobind(_, _2, descriptor) {
         }
     };
     return adjDescriptor;
-}
-//ProjectInput Class
+};
+// ProjectInput Class
 var ProjectInput = /** @class */ (function () {
     function ProjectInput() {
-        //Grabs the template element that we want to render
-        //In this case it is project input
         this.templateElement = document.getElementById('project-input');
-        //Grabs the div that we will render our form to.
         this.hostElement = document.getElementById('app');
         var importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild;
@@ -41,7 +38,6 @@ var ProjectInput = /** @class */ (function () {
         this.element.addEventListener('submit', this.submitHandler);
     };
     ProjectInput.prototype.attach = function () {
-        //Insert right after opending div tag on host element
         this.hostElement.insertAdjacentElement('afterbegin', this.element);
     };
     __decorate([
--- src/app.ts
+++ src/app.ts
@@ -1,67 +1,70 @@
-//autobind decorater
-
-function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
-
+// autobind decorator
+const autobind =(
+    _: any,
+    _2: string,
+    descriptor: PropertyDescriptor
+  ) => {
     const originalMethod = descriptor.value;
     const adjDescriptor: PropertyDescriptor = {
-        configurable: true,
-
-        get() {
-            const boundFn = originalMethod.bind(this)
-            return boundFn;
-        }
-    }
+      configurable: true,
+      get() {
+        const boundFn = originalMethod.bind(this);
+        return boundFn;
+      }
+    };
     return adjDescriptor;
-}
-
-//ProjectInput Class
-
-class ProjectInput {
-// Template Element is declared as an HTML element
+  }
+  
+  // ProjectInput Class
+  class ProjectInput {
     templateElement: HTMLTemplateElement;
-    //Host Element will be the div that will host the rendered version of our app
     hostElement: HTMLDivElement;
     element: HTMLFormElement;
     titleInputElement: HTMLInputElement;
     descriptionInputElement: HTMLInputElement;
     peopleInputElement: HTMLInputElement;
-
-
-
-
+  
     constructor() {
-        //Grabs the template element that we want to render
-        //In this case it is project input
-        this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
-        //Grabs the div that we will render our form to.
-        this.hostElement = document.getElementById('app')! as HTMLDivElement;
-
-        const importedNode = document.importNode(this.templateElement.content, true);
-        this.element = importedNode.firstElementChild as HTMLFormElement;
-        this.element.id = 'user-input';
-
-
-        this.titleInputElement = this.element.querySelector('#title') as HTMLInputElement;
-        this.descriptionInputElement = this.element.querySelector('#description') as HTMLInputElement;
-        this.peopleInputElement = this.element.querySelector('#people') as HTMLInputElement;
-
-        this.configure();
-        this.attach();
+      this.templateElement = document.getElementById(
+        'project-input'
+      )! as HTMLTemplateElement;
+      this.hostElement = document.getElementById('app')! as HTMLDivElement;
+  
+      const importedNode = document.importNode(
+        this.templateElement.content,
+        true
+      );
+      this.element = importedNode.firstElementChild as HTMLFormElement;
+      this.element.id = 'user-input';
+  
+      this.titleInputElement = this.element.querySelector(
+        '#title'
+      ) as HTMLInputElement;
+      this.descriptionInputElement = this.element.querySelector(
+        '#description'
+      ) as HTMLInputElement;
+      this.peopleInputElement = this.element.querySelector(
+        '#people'
+      ) as HTMLInputElement;
+  
+      this.configure();
+      this.attach();
     }
-
+  
     @autobind
-    private submitHandler(event: Event){
-        event.preventDefault();
-        console.log(this.titleInputElement.value)
+    private submitHandler(event: Event) {
+      event.preventDefault();
+      console.log(this.titleInputElement.value);
     }
-
+  
     private configure() {
-        this.element.addEventListener('submit', this.submitHandler)
+      this.element.addEventListener('submit', this.submitHandler);
     }
-
+  
     private attach() {
-        //Insert right after opending div tag on host element
-        this.hostElement.insertAdjacentElement('afterbegin', this.element);
+      this.hostElement.insertAdjacentElement('afterbegin', this.element);
     }
-}
-const prjInput = new ProjectInput();
+  }
+  
+  const prjInput = new ProjectInput();
+  
